e009ae77157154ef0e0a998659f533cbca5f7c77
ShubhamSingh <shubham.singh@hashedin.com>
2017-07-29 07:59:58 +0530

    styling the ui

--- Listner/src/App.js
+++ Listner/src/App.js
@@ -4,13 +4,21 @@ import ApiAi from './ApiAi';
 const client = new ApiAi.ApiAiClient({accessToken: '47da946ec0f441728422eb1a61d2ff20'});
 
 class App extends Component {
+  constructor() {
+    super();
+    this.toggle = true;
+  }
+  
   speech = () => {
     var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
     recognition.lang = 'en-US';
     recognition.interimResults = false;
     recognition.maxAlternatives = 5;
-    recognition.start();
-
+    if(this.toggle)
+      recognition.start();
+    else
+      recognition.stop();
+    this.toggle = !this.toggle;
     recognition.onresult = function(event) {
       client.textRequest(event.results[0][0].transcript).then(result => console.log(result));
     };
@@ -20,7 +28,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <button onClick={this.speech}>speech</button>
+          <h1>Speech To Code</h1>  
+        </div>
+        <div className="App-Body">
+          <div>
+            <h2>Tap To Begin</h2>
+            <div className="speech-btn"><button onClick={this.speech}></button></div>
+          </div>
         </div>
       </div>
     );
