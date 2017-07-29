6d185e0497ffa2cbceadebbd0cbf9850f6f289a4
pansul.bhatt <pansul.bhatt@hashedin.com>
2017-07-29 11:31:43 +0530

    Adding new key

--- Listner/src/App.js
+++ Listner/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 import ApiAi from './ApiAi';
-const client = new ApiAi.ApiAiClient({accessToken: '47da946ec0f441728422eb1a61d2ff20'});
+const client = new ApiAi.ApiAiClient({accessToken: 'd0fbd87be1ce4078b36e85497b2fb440'});
 
 class App extends Component {
   constructor() {
